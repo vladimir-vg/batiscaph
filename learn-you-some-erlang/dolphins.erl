236e33d482febee58ba24f6df57e709d0ab63a4b
Vladimir Gordeev <gordeev.vladimir.v@gmail.com>
2017-05-03 11:59:58 +0300

    add dolphins example from book and repl session, LICENSE and README

---
+++ learn-you-some-erlang/dolphins.erl
@@ -0,0 +1,34 @@
+-module(dolphins).
+-compile(export_all).
+
+dolphin1() ->
+    receive
+        do_a_flip ->
+            io:format("How about no?~n");
+        fish ->
+            io:format("So long and thanks for all the fish!~n");
+        _ ->
+            io:format("Heh, we're smarter than you humans.~n")
+    end.
+
+dolphin2() ->
+    receive
+        {From, do_a_flip} ->
+            From ! "How about no?";
+        {From, fish} ->
+            From ! "So long and thanks for all the fish!";
+        _ ->
+            io:format("Heh, we're smarter than you humans.~n")
+    end.
+
+dolphin3() ->
+    receive
+        {From, do_a_flip} ->
+            From ! "How about no?",
+            dolphin3();
+        {From, fish} ->
+            From ! "So long and thanks for all the fish!";
+        _ ->
+            io:format("Heh, we're smarter than you humans.~n"),
+            dolphin3()
+    end.
