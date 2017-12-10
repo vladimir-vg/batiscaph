caf20a3e0ed2195327b1dea6cd7639c442270f33
Vladimir Gordeev <gordeev.vladimir.v@gmail.com>
2017-12-10 19:16:13 +0300

    display links tree for different contexts on hover

--- priv/wwwroot/app/MainPage.jsx
+++ priv/wwwroot/app/MainPage.jsx
@@ -4,8 +4,13 @@ class MainPage extends React.Component {
   constructor() {
     super();
     this.state = {
-      items: []
+      items: [],
+      selectedIndex: null
     };
+
+    // because new EcmaScript standard is poorly designed
+    // we have to do bindings like that
+    this.onBlockLeave = this.onBlockLeave.bind(this);
   }
 
   componentDidMount() {
@@ -20,20 +25,72 @@ class MainPage extends React.Component {
     });
   }
 
+  onLinkEnter(i) {
+    this.setState({selectedIndex: i});
+  }
+
+  onBlockLeave() {
+    this.setState({selectedIndex: null});
+  }
+
+  renderRecursiveTree(path, tree) {
+    if (!tree) { return null; }
+
+    let links = [];
+    for (var key in tree) {
+      const path1 = path + '/' + key;
+      
+      links.push(<li key={key}>
+          <Link to={path + '/' + key}>{key}</Link>
+          {this.renderRecursiveTree(path1, tree[key])}
+        </li>
+      );
+    }
+    return <ul>
+      {links}
+    </ul>;
+  }
+
+  renderHoveredContexts() {
+    if (!this.state.selectedIndex) {
+      return null;
+    }
+    const id = this.state.items[this.state.selectedIndex][0];
+    const tree = this.state.items[this.state.selectedIndex][1];
+    const path = '/scenarios2/'+id;
+    return this.renderRecursiveTree(path, tree);
+  }
+
   render() {
     let links = [];
     for (const i in this.state.items) {
-      let id = this.state.items[i].instance_id;
-      links.push(<div key={i}><Link to={'/scenarios2/'+id} className="item">{id}</Link></div>);
+      let id = this.state.items[i][0];
+      let className = "scenario-item";
+      if (i == this.state.selectedIndex) {
+        className += " selected";
+      }
+      links.push(
+        <div key={i}>
+          <Link to={'/scenarios2/'+id} onMouseEnter={this.onLinkEnter.bind(this, i)} className={className}>
+            {id}
+          </Link>
+        </div>
+      );
     }
 
-    return <div className="content-page">
+    return <div id="MainPage" className="content-page">
       <div className="head-block">
         <button className="btn" onClick={this.props.startNewShell}>Start new shell</button>
       </div>
       <br />
-      <div className="scenarios-list-block">
-        {links}
+      <h1>Previous sessions:</h1>
+      <div style={{display: 'flex'}} onMouseLeave={this.onBlockLeave}>
+        <div style={{padding: 10}} className="scenarios-list-block">
+          {links}
+        </div>
+        <div style={{flex: '1', padding: 10}}>
+          {this.renderHoveredContexts()}
+        </div>
       </div>
     </div>;
   }
