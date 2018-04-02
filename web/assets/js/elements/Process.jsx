b8f4a88ac47a6ffd73265520d8887afb0f2ea492
Vladimir Gordeev <gordeev.vladimir.v@gmail.com>
2018-04-02 11:10:06 +0300

    display spawn lines

--- web/assets/js/elements/Process.jsx
+++ web/assets/js/elements/Process.jsx
@@ -4,8 +4,9 @@ import PropTypes from 'prop-types';
 import c from '../constraint';
 
 
-const PROC_WIDTH = 4;
 
+const PROC_WIDTH = 4;
+const EXIT_HEIGHT = 2;
 class Component extends React.Component {
   render() {
     const g = this.props.grid;
@@ -17,13 +18,24 @@ class Component extends React.Component {
 
     let exit = null;
     if (this.props.exitedY) {
-      const y = g.yRowAt(this.props.exitedY-1);
-      exit = <rect className="exit" x={x} y={y} width={width} height={g.yRowHeight} />
+      const y = g.yRowAt(this.props.exitedY)-EXIT_HEIGHT/2;
+      exit = <rect className="exit" x={x} y={y} width={width} height={EXIT_HEIGHT} />
+    }
+
+    let spawnLines = null;
+    if (typeof this.props.parentX === 'number') { // might be zero
+      let parentX = Math.floor(g.xColWidth/2) + g.xColStart(this.props.parentX);
+      let parentY1 = y - g.yRowHeight;
+      spawnLines = <g>
+        <line className="spawn" x1={x} y1={y+0.5} x2={parentX} y2={y+0.5} />
+        <line className="spawn" x1={parentX+0.5} y1={y+0.5} x2={parentX+0.5} y2={parentY1+0.5} />
+      </g>;
     }
 
     return <g className="Process">
       <rect className="body" x={x} y={y} width={width} height={height} />
       {exit}
+      {spawnLines}
     </g>;
   }
 }
