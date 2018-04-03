b45b225b1ac9231793d9d360dd09aaafbeb0c2f7
Vladimir Gordeev <gordeev.vladimir.v@gmail.com>
2018-04-03 16:12:12 +0300

    add official cowboy examples into test app

--- test/erlang_app1/src/erlang_app1_app.erl
+++ test/erlang_app1/src/erlang_app1_app.erl
@@ -18,8 +18,18 @@ stop(_State) ->
 
 start_cowboy(Port) ->
   application:ensure_all_started(cowboy),
+
+  Table = ets:new(stream_tab, []),
+  generate_rows(Table, 1000),
+
   Dispatch = cowboy_router:compile([
     {'_', [
+      % official Cowboy examples
+      {"/rest_basic_auth", rest_basic_auth, []},
+      {"/rest_hello_world", rest_hello_world, []},
+      {"/rest_pastebin/[:paste_id]", rest_pastebin, []},
+      {"/rest_stream_response/[:v1]", [{v1, int}], rest_stream_response, Table},
+
       {"/spawn_process", spawn_process, []},
       {"/hello_world", hello_world, []}
     ]}
@@ -28,3 +38,17 @@ start_cowboy(Port) ->
     {env, [{dispatch, Dispatch}]}
   ]),
   ok.
+
+
+
+generate_rows(_Table, 0) ->
+  ok;
+generate_rows(Table, N) ->
+  ets:insert(Table, {key(), val(), val()}),
+  generate_rows(Table, N - 1).
+
+key() -> key(10).
+key(N) -> key(<< (random:uniform(26) - 1) >>, N - 1).
+key(Acc, 0) -> binary_part(base64:encode(Acc), 0, 8);
+key(Acc, N) -> key(<< Acc/binary, (random:uniform(26) - 1) >>, N - 1).
+val() -> random:uniform(50).
