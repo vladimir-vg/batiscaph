2027008b28c1e9ed540876db3a4c805babbd5a53
Vladimir Gordeev <gordeev.vladimir.v@gmail.com>
2018-06-04 11:07:58 +0300

    remove unused code from test erlang17_app1

--- test/erlang17_app1/src/erlang17_app1_app.erl
+++ test/erlang17_app1/src/erlang17_app1_app.erl
@@ -19,21 +19,9 @@ stop(_State) ->
 start_cowboy(Port) ->
   application:ensure_all_started(cowboy),
 
-  Table = ets:new(stream_tab, []),
-  generate_rows(Table, 1000),
-
   Dispatch = cowboy_router:compile([
     {'_', [
       {"/tree_testcases/:testcase_name", tree_testcase_handler, []},
-
-      % official Cowboy examples
-      {"/rest_basic_auth", rest_basic_auth, []},
-      {"/rest_hello_world", rest_hello_world, []},
-      {"/rest_pastebin/[:paste_id]", rest_pastebin, []},
-      {"/rest_stream_response/[:v1]", [{v1, int}], rest_stream_response, Table},
-
-      {"/spawn_process", spawn_process, []},
-      {"/subscribe_to_process_info", subscribe_to_process_info, []},
       {"/hello_world", hello_world, []}
     ]}
   ]),
@@ -41,17 +29,3 @@ start_cowboy(Port) ->
     {env, [{dispatch, Dispatch}]}
   ]),
   ok.
-
-
-
-generate_rows(_Table, 0) ->
-  ok;
-generate_rows(Table, N) ->
-  ets:insert(Table, {key(), val(), val()}),
-  generate_rows(Table, N - 1).
-
-key() -> key(10).
-key(N) -> key(<< (random:uniform(26) - 1) >>, N - 1).
-key(Acc, 0) -> binary_part(base64:encode(Acc), 0, 8);
-key(Acc, N) -> key(<< Acc/binary, (random:uniform(26) - 1) >>, N - 1).
-val() -> random:uniform(50).
